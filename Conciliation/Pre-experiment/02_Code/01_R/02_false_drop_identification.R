fd0da0b89a1786e0c24bce6821f0b27fffd464c1
Antonio Carbonell <antoniocarbonell@live.com.mx>
2024-08-28 14:00:51 -0600

    Add a preliminary count of repeated curps
    Filter for curp and counted in how many casefiles that worker has more than one request.

--- Conciliation/Pre-experiment/02_Code/01_R/02_false_drop_identification.R
+++ Conciliation/Pre-experiment/02_Code/01_R/02_false_drop_identification.R
@@ -57,5 +57,33 @@ final_base <- left_join(hearings_features,worker_hearing_request_characteristics
 # The possibility of a mistmath is confusing, as there should be data for every worker. 
 # But remember on 01_clean_conciliation we winsorized workers data.
 
+# -----------------------------------------------------------------------------
+#                               Identification
+# -----------------------------------------------------------------------------
+
+# To identify a false drop, it is necessary to have the curp of the worker, as it is the only intifier that lets us know if 
+# a worker went twice to the conciliaiton center 
+
+# filtering for curp 
+final_base <- final_base %>%
+  filter(!is.na(curp_trabajador))
+
+# prelimnar count of worker repetition on the data set 
+final_base <- final_base %>%
+  group_by(curp_trabajador) %>%
+  mutate(cuanto_repiten_curp_trabajador=n()) %>%
+  ungroup() %>%
+  group_by(rfc_trabajador) %>%
+  mutate(cuanto_repiten_rfc_trabajador=n()) %>%
+  ungroup() 
+
+# is this curp repeated somewhere in the data set dummy
+final_base <- final_base %>%
+  mutate(curp_repetido=if_else(cuanto_repiten_curp_trabajador>1,1,0))
+
+#  number of cases where we have a worker that came more than once (take into account that they are counting double or tripple
+# , the data is at the level hearing so every hearing where I "Antonio" came to the heraing counts as once observation)
+
+ table(final_base$curp_repetido)
 
 
